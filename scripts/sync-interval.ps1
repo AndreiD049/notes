5ee67880fefd684f924b47807598a3ee807a1dff
AndreiD049 <andrei.dimitrascu.94@gmail.com>
2023-01-22 11:45:43 +0200

    Wiki sync. 2023-01-22T11:45:42

--- scripts/sync-interval.ps1
+++ scripts/sync-interval.ps1
@@ -12,7 +12,7 @@ catch {
 
 if ($git_installed) {
     do {
-        Start-Sleep -Seconds 20
+        Start-Sleep -Seconds 100
         & powershell -File ./scripts/sync.ps1
     } until ($infinity)
 } else {
