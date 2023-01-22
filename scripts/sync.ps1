c4575011190938c60bc841624a067ea451a4f17a
AndreiD049 <andrei.dimitrascu.94@gmail.com>
2023-01-22 11:25:35 +0200

    Wiki sync. 2023-01-22T11:25:34

--- scripts/sync.ps1
+++ scripts/sync.ps1
@@ -1,11 +1,12 @@
 Set-Location (Get-Item $PSScriptRoot).Parent.FullName
 
 $status = git.exe status
+$log_file = "./synclog.txt"
 if (-not ($status -match "nothing to commit")) { 
-    $status > "./synclog.txt"
+    $status | Add-Content $log_file
     $commit_message = "Wiki sync. $(Get-Date -Format "yyyy-MM-ddThh:mm:ss")"
-    & git.exe add . > "./synclog.txt"
-    & git.exe commit -m $commit_message > "./synclog.txt"
+    & git.exe add . | Add-Content $log_file
+    & git.exe commit -m $commit_message | Add-Content $log_file
 } else {
     Write-Output "Nothing to commit";
 }
