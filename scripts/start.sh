c54a87bdddb31d55cd1041690c6773a59b8edc5e
AndreiD049 <andrei.dimitrascu.94@gmail.com>
2023-01-29 10:50:03 +0200

    Notes sync. 2023-01-29T10:50:01

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,1 +1,3 @@
+cd ~/Documents/notes/
+git pull origin main
 npx tiddlywiki ~/Documents/notes/data/Default --listen port=3000 &>/dev/null
--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,4 +1,5 @@
 DAT=$(date '+%Y-%m-%dT%H:%M:%S')
+git pull origin main
 git add .
 git commit -m "Notes sync. $DAT"
 git push origin main
